8dceff0883044f40159bf138eb1cab2d77d58dbc
yt910 <83634544+yt910@users.noreply.github.com>
2023-06-18 02:21:55 +0800

    Add files via upload

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>ChatGPT</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          开心快乐每一天
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
